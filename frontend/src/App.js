371136f4e55f815ed3164ed9bbc105de69cd3ae7
Neil Dahlke <neil.dahlke@gmail.com>
2020-06-22 20:16:51 -0700

    Working example.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,131 @@
 import React, { Component } from 'react';
-
-// import AllResortsChart from './charts/AllResortsChart';
+import logo from './svg/logo_black.svg';
 import './App.scss';
 
+const HOST = window.location.hostname;
+// const PORT = window.location.port;
+const PORT = 5000;
+const CONFIG_BUNDLES_URL = `http://${HOST}:${PORT}/config_bundles`;
+const WORKSPACES_URL = `http://${HOST}:${PORT}/workspaces`;
+
+
 class App extends Component {
-  // <AllResortsChart />
+
+  constructor(props) {
+    super(props);
+    const initState = {
+      configBundles: {},
+      workspaces: []
+    };
+    this.state = initState;
+    this._fetchConfigBundles();
+    this._fetchWorkspaces();
+  }
+
+  _fetchConfigBundles() {
+      fetch(CONFIG_BUNDLES_URL)
+        .then((response) => {
+          return response.json()
+        })
+        .catch((err) => {
+          console.log("Error retrieving config bundle data.");
+        })
+        .then((data) => {
+          const newState = {
+            configBundles: data
+          }
+          this.setState(newState);
+        });
+  }
+
+  _fetchWorkspaces() {
+      fetch(WORKSPACES_URL)
+        .then((response) => {
+          return response.json()
+        })
+        .catch((err) => {
+          console.log("Error retrieving workspaces data.");
+        })
+        .then((data) => {
+          const newState = {
+            workspaces: data
+          }
+          this.setState(newState);
+        });
+  }
+
+  _createFromConfigBundle(bundleId) {
+      const url = `${CONFIG_BUNDLES_URL}/${bundleId}`;
+      console.log("create from bundle", url)
+      fetch(url)
+        .then((response) => {
+          return response.json()
+        })
+        .catch((err) => {
+          console.error("Error retrieving config bundle data.");
+        })
+        .then((data) => {
+          console.log("create response", data);
+        });
+  }
+
+  _buildConfigBundleElements() {
+    let configBundleElements = {};
+    // TODO: add a create button
+    // TODO: add a button to link to VCS so you can see what you're doing
+    for (var key in this.state.configBundles) {
+      configBundleElements[key] = [];
+      configBundleElements[key] = this.state.configBundles[key].map((data) => {
+        return (
+          <div className="config-bundle" key={data.id} data-tf-config-bundle-id={data.id} onClick={this._clickConfigBundle.bind(this)}>
+            {data.name}
+          </div>
+        );
+      })
+    }
+    return configBundleElements;
+  }
+
+  _buildWorkspaceElements() {
+    // TODO: add status badge
+    const workspaceElements = this.state.workspaces.map((data) => {
+        console.log(data);
+        return (
+          <div className="workspace" key={data.id} data-workspace-id={data.id} onClick={this._clickWorkspace.bind(this)}>
+            {data.attributes.name}
+          </div>
+        );
+    });
+    return workspaceElements;
+  }
+
+
+  _clickConfigBundle(event) {
+    const bundleId = event.currentTarget.dataset.tfConfigBundleId;
+    this._createFromConfigBundle(bundleId);
+  }
+
+  _clickWorkspace(event) {
+    console.log("TODO", event);
+  }
+
 
   render() {
+    const configBundleElements = this._buildConfigBundleElements();
+    const workspaceElements = this._buildWorkspaceElements();
     return (
       <div className="App">
-        <div className="infra-set">
-          Demo Spark Cluster
+        <img src={logo} className="logo" alt="logo" />
+        <h1>TFC Self Service Demo</h1>
+        <div className="half-col config-bundles">
+          <h2>TF Bundle Catalog</h2>
+          {configBundleElements["aws"]}
+          {configBundleElements["azure"]}
+          {configBundleElements["gcp"]}
         </div>
-        <div className="infra-set">
-          Demo 3 Tier Web App (RDS, Route 53, EC2 Instance)
-        </div>
-        <div className="infra-set">
-          Dynamo
-        </div>
-        <div className="infra-set">
-          Spanner
-        </div>
-        <div className="infra-set">
-          Cosmos
-        </div>
-        <div className="infra-set">
-          Managed Kubernetes in all 3 majors.
+        <div className="half-col workspaces">
+          <h2>TF Workspaces </h2>
+          {workspaceElements}
         </div>
       </div>
     );
--- main.py
+++ main.py
@@ -1,23 +1,93 @@
 #!/usr/bin/python3
 import os
 import json
+import time
 import socket
 from flask import Flask, send_from_directory, jsonify
 from flask_cors import CORS
-# from services.snowfall import ServiceSnowfall
-# from util.db import PGDB
+from terrasnek.api import TFC
 
 app = Flask(__name__, static_folder='react_app/build')
 CORS(app)
-# pgdb = PGDB()
-CONTAINER_ID = socket.gethostname()
+TFC_ORG_NAME="hc-se-tfe-demo-neil"
+TFC_OAUTH_TOKEN_ID = os.getenv("TFC_OAUTH_TOKEN_ID", None)
+TFC_TOKEN = os.getenv("TFC_TOKEN", None)
 
+# TODO: add as many other bundles as I can, including the Omni demo
+CONFIG_BUNDLES = {
+    "aws": [
+        {
+            "id": "aws-two-tier",
+            "working-dir": "two-tier-tfc-demo-app/aws",
+            "name": "AWS Two Tier App"
+        }
+    ],
+    "azure": [
+        {
+            "id": "azure-two-tier",
+            "working-dir": "two-tier-tfc-demo-app/azure",
+            "name": "Azure Two Tier App"
+        }
+    ],
+    "gcp": [
+        {
+            "id": "gcp-two-tier",
+            "working-dir": "two-tier-tfc-demo-app/gcp",
+            "name": "GCP Two Tier App"
+        }
+    ]
+}
 
-@app.route('/snowfall/<resort_name>')
-def snowfall(resort_name):
-    # snow_srv = ServiceSnowfall(pgdb)
-    return jsonify(snow_srv.get_snowfall_for_resort(resort_name))
+def _get_create_payload(bundle_id):
+    # TODO: fix workspace name
 
+    working_dir = None
+    for provider in CONFIG_BUNDLES:
+        for bundle in CONFIG_BUNDLES[provider]:
+            print(bundle)
+            if bundle["id"] == bundle_id:
+                working_dir = bundle["working-dir"]
+                break
+
+    workspace_name = f"{bundle_id}-{int(time.time())}"
+
+    return {
+        "data": {
+            "attributes": {
+                "name": workspace_name, 
+                "terraform_version": "0.12.24",
+                "working-directory": working_dir,
+                "vcs-repo": {
+                    "identifier": "dahlke/tfc-demo",
+                    "oauth-token-id": TFC_OAUTH_TOKEN_ID,
+                    "branch": "master"
+                }
+            },
+            "type": "workspaces"
+        }
+    }
+
+@app.route('/config_bundles/')
+def config_bundles():
+    return jsonify(CONFIG_BUNDLES)
+
+# TODO: apply by ID function which I can even enter manually.
+@app.route('/workspaces/')
+def list_workspaces():
+    api = TFC(TFC_TOKEN)
+    api.set_org(TFC_ORG_NAME)
+    all_workspaces = api.workspaces.list()["data"]
+    print("get the workspaces")
+    return jsonify(all_workspaces)
+
+@app.route('/config_bundles/<bundle_id>')
+def create_bundle(bundle_id):
+    api = TFC(TFC_TOKEN)
+    api.set_org(TFC_ORG_NAME)
+    # TODO: webhook receive, explain why I'm not applying
+    create_payload = _get_create_payload(bundle_id)
+    workspace = api.workspaces.create(create_payload)["data"]
+    return jsonify(workspace)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
